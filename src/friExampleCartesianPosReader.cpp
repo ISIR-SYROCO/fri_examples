beb9860fa81444523987eb486aeb0ec6d2a3da91
shak <sovannara.hak@isir.upmc.fr>
2014-01-15 13:57:31 +0100

    Read Cartesian position example

--- src/friExampleCartesianPosReader.cpp
+++ src/friExampleCartesianPosReader.cpp
@@ -12,10 +12,25 @@ FriExampleCartesianPosReader::FriExampleCartesianPosReader(std::string const& na
 }
 
 FriExampleCartesianPosReader::~FriExampleCartesianPosReader(){
-
 }
 
 void FriExampleCartesianPosReader::updateHook(){
+    geometry_msgs::Pose cartPosData;
+    RTT::FlowStatus cartPosFS = iport_cart_pos.read(cartPosData);
+
+    if(cartPosFS == RTT::NewData){
+        std::cout << "Position end effector [x, y, z]:" << std::endl;
+        std::cout << "[" << cartPosData.position.x
+                  << ", " << cartPosData.position.y 
+                  << ", " << cartPosData.position.z
+                  << "]" << std::endl;
+        std::cout << "Orientation end effector [x, y, z, w]:" << std::endl;
+        std::cout << "[" << cartPosData.orientation.x
+                  << ", " << cartPosData.orientation.y
+                  << ", " << cartPosData.orientation.z
+                  << ", " << cartPosData.orientation.w
+                  << "]" << std::endl;
+    }
 }
 
 ORO_CREATE_COMPONENT(FriExampleCartesianPosReader)
