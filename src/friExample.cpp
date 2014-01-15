054b633e82f345b7f43290c99dfd4a2cab44f090
shak <sovannara.hak@isir.upmc.fr>
2014-01-15 17:55:43 +0100

    Add get cartesian frame

--- src/friExample.cpp
+++ src/friExample.cpp
@@ -67,6 +67,22 @@ void FriExample::getFRIJointState(){
 }
 
 void FriExample::getCartesianPosition(){
+    geometry_msgs::Pose cartPosData;
+    RTT::FlowStatus cartPosFS = iport_cart_pos.read(cartPosData);
+
+    if(cartPosFS == RTT::NewData){
+        std::cout << "Position end effector [x, y, z]:" << std::endl;
+        std::cout << "[" << cartPosData.position.x
+                  << ", " << cartPosData.position.y 
+                  << ", " << cartPosData.position.z
+                  << "]" << std::endl;
+        std::cout << "Orientation end effector [x, y, z, w]:" << std::endl;
+        std::cout << "[" << cartPosData.orientation.x
+                  << ", " << cartPosData.orientation.y
+                  << ", " << cartPosData.orientation.z
+                  << ", " << cartPosData.orientation.w
+                  << "]" << std::endl;
+    }
 }
 
 void FriExample::getCartesianFrame(){
