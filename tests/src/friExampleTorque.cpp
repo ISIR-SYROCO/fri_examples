84b54b5e4ae48cea1bcceb8bb0332f10233a3268
shak <sovannara.hak@isir.upmc.fr>
2014-02-03 11:02:19 +0100

    Do not send command if the control strategy is incorrect in friExampleTorque

--- tests/src/friExampleTorque.cpp
+++ tests/src/friExampleTorque.cpp
@@ -29,26 +29,27 @@ bool FriExampleTorque::doStart(){
 }
 
 void FriExampleTorque::updateHook(){
-    if(requiresControlMode(30)){
-        lwr_fri::FriJointState fri_joint_state_data;
-        RTT::FlowStatus fs = iport_fri_joint_state.read(fri_joint_state_data);
-        if(fs == RTT::NewData){
+    lwr_fri::FriJointState fri_joint_state_data;
+    RTT::FlowStatus fs = iport_fri_joint_state.read(fri_joint_state_data);
+    if(fs == RTT::NewData){
 
-            //The controller makes an interpolation between msrJntPos and cmdJntPos
-            //to generate a trajectory. However, the distance between them has to be small
-            //so that the generated trajectory does not violate velocities limits.
-            //So we get current joint position fri_joint_state_data.msrJntPos
-            //and send it back to the controller in order to keep a msrJntPos and cmdJntPos close.
-            motion_control_msgs::JointPositions joint_position_command;
-            joint_position_command.positions.assign(7, 0.0);
-            for(unsigned int i = 0; i < LWRDOF; i++){
-                joint_position_command.positions[i] = fri_joint_state_data.msrJntPos[i];
-            }
+        //The controller makes an interpolation between msrJntPos and cmdJntPos
+        //to generate a trajectory. However, the distance between them has to be small
+        //so that the generated trajectory does not violate velocities limits.
+        //So we get current joint position fri_joint_state_data.msrJntPos
+        //and send it back to the controller in order to keep a msrJntPos and cmdJntPos close.
+        motion_control_msgs::JointPositions joint_position_command;
+        joint_position_command.positions.assign(7, 0.0);
+        for(unsigned int i = 0; i < LWRDOF; i++){
+            joint_position_command.positions[i] = fri_joint_state_data.msrJntPos[i];
+        }
 
-            motion_control_msgs::JointEfforts joint_eff_command;
+        motion_control_msgs::JointEfforts joint_eff_command;
 
-            joint_eff_command.efforts.assign(LWRDOF, 0.0); 
-            joint_eff_command.efforts[2] = torque;
+        joint_eff_command.efforts.assign(LWRDOF, 0.0); 
+        joint_eff_command.efforts[2] = torque;
+
+        if(requiresControlMode(30)){
             oport_joint_efforts.write(joint_eff_command);
             oport_joint_position.write(joint_position_command);
         }
